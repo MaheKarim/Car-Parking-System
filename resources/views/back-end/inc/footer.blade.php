0069140b728467002530517b32ca351c94978068
MaheKarim <mahekarim@gmail.com>
2019-04-02 14:10:12 +0600

    Backend File Integrated

---
+++ resources/views/back-end/inc/footer.blade.php
@@ -0,0 +1,16 @@
+<!-- Bootstrap core JavaScript-->
+<script src="{{ asset('/') }}back-end/vendor/jquery/jquery.min.js"></script>
+<script src="{{ asset('/') }}back-end/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<!-- Core plugin JavaScript-->
+<script src="{{ asset('/') }}back-end/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+<!-- Custom scripts for all pages-->
+<script src="{{ asset('/') }}back-end/js/sb-admin-2.min.js"></script>
+
+<!-- Page level plugins -->
+<script src="{{ asset('/') }}back-end/vendor/chart.js/Chart.min.js"></script>
+
+<!-- Page level custom scripts -->
+<script src="{{ asset('/') }}back-end/js/demo/chart-area-demo.js"></script>
+<script src="{{ asset('/') }}back-end/js/demo/chart-pie-demo.js"></script>
